c0bc3e4c59cdf6c7ce674bd115610cc0a09d4dd1
Cato Mott II <cmott@bioplusrx.com>
2019-03-21 14:06:15 -0400

    Added ValidCoin, CoinValue, CoinReturn, AddAmount, and Display methods.

--- Company.VendingMachine/Company.VendingMachine.Tests/VendingMachinTests.cs
+++ Company.VendingMachine/Company.VendingMachine.Tests/VendingMachinTests.cs
@@ -1,22 +1,173 @@
+using System;
 using Xunit;
 
 namespace Company.VendingMachine.Tests
 {
     public class VendingMachinTests
     {
-        private VendingMachine _vendingMachine;
+        private readonly VendingMachine _vendingMachine;
+        private readonly Coin _penny = new Coin(2, 2);
+        private readonly Coin _nickle = new Coin(3, 3);
+        private readonly Coin _dime = new Coin(1, 1);
+        private readonly Coin _quarter = new Coin(4, 4);
 
         public VendingMachinTests()
         {
             _vendingMachine = new VendingMachine();
         }
 
-        [Fact]
-        public void ValidCoin()
-        {
-            Coin dime = new Coin(1,1);
+        #region Accept Coins
 
-            Assert.Equal(true, _vendingMachine.InsertCoin(dime));
+        [Fact]
+        public void ValidCoinPenny()
+        {
+            Assert.False(_vendingMachine.ValidCoin(_penny));
         }
+
+        [Fact]
+        public void ValidCoinNickle()
+        {
+            Assert.True(_vendingMachine.ValidCoin(_nickle));
+        }
+
+        [Fact]
+        public void ValidCoinDime()
+        {
+            Assert.True(_vendingMachine.ValidCoin(_dime));
+        }
+
+        [Fact]
+        public void ValidCoinQuarter()
+        {
+            Assert.True(_vendingMachine.ValidCoin(_quarter));
+        }
+
+
+        [Fact]
+        public void CoinValuePenny()
+        {
+            Assert.Equal(0.01M, _vendingMachine.CoinValue(_penny));
+        }
+
+        [Fact]
+        public void CoinValueNickel()
+        {
+            Assert.Equal(0.05M, _vendingMachine.CoinValue(_nickle));
+        }
+
+        [Fact]
+        public void CoinValueDime()
+        {
+            Assert.Equal(0.10M, _vendingMachine.CoinValue(_dime));
+        }
+
+        [Fact]
+        public void CoinValueQuarter()
+        {
+            Assert.Equal(0.25M, _vendingMachine.CoinValue(_quarter));
+        }
+
+        [Fact]
+        public void AddAmountPennyPlusPenny()
+        {
+            _vendingMachine.InsertCoin(_penny);
+            _vendingMachine.InsertCoin(_penny);
+            Assert.Equal(0.00M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void AddAmountPennyPlusNickle()
+        {
+            _vendingMachine.InsertCoin(_penny);
+            _vendingMachine.InsertCoin(_nickle);
+            Assert.Equal(0.05M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void AddAmountPennyPlusDime()
+        {
+            _vendingMachine.InsertCoin(_penny);
+            _vendingMachine.InsertCoin(_dime);
+            Assert.Equal(0.10M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void AddAmountPennyPlusQuarter()
+        {
+            _vendingMachine.InsertCoin(_penny);
+            _vendingMachine.InsertCoin(_quarter);
+            Assert.Equal(0.25M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void AddAmountNicklePlusNickle()
+        {
+            _vendingMachine.InsertCoin(_nickle);
+            _vendingMachine.InsertCoin(_nickle);
+            Assert.Equal(0.10M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void AddAmountNicklePlusDime()
+        {
+            _vendingMachine.InsertCoin(_nickle);
+            _vendingMachine.InsertCoin(_dime);
+            Assert.Equal(0.15M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void AddAmountNicklePlusQuarter()
+        {
+            _vendingMachine.InsertCoin(_nickle);
+            _vendingMachine.InsertCoin(_quarter);
+            Assert.Equal(0.30M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void AddAmountDimePlusDime()
+        {
+            _vendingMachine.InsertCoin(_dime);
+            _vendingMachine.InsertCoin(_dime);
+            Assert.Equal(0.20M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void AddAmountDimePlusQuarter()
+        {
+            _vendingMachine.InsertCoin(_dime);
+            _vendingMachine.InsertCoin(_quarter);
+            Assert.Equal(0.35M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void AddAmountQuarterPlusQuarter()
+        {
+            _vendingMachine.InsertCoin(_quarter);
+            _vendingMachine.InsertCoin(_quarter);
+            Assert.Equal(0.50M, _vendingMachine.Amount);
+        }
+
+        [Fact]
+        public void DisplayInsetCoinNoCoin()
+        {
+            Assert.Equal("INSERT COIN", _vendingMachine.Display(Convert.ToDecimal(_vendingMachine.Amount)));
+        }
+
+        [Fact]
+        public void DisplayInsetCoinNicklePlusDimePlusQuarter()
+        {
+            _vendingMachine.InsertCoin(_nickle);
+            _vendingMachine.InsertCoin(_dime);
+            _vendingMachine.InsertCoin(_quarter);
+            Assert.Equal("0.40", _vendingMachine.Display(Convert.ToDecimal(_vendingMachine.Amount)));
+        }
+
+        [Fact]
+        public void CoinReturn()
+        {
+            _vendingMachine.InsertCoin(_penny);
+            Assert.True(_vendingMachine.CoinReturn());
+        }
+        #endregion
     }
 }
--- Company.VendingMachine/Company.VendingMachine/VendingMachine.cs
+++ Company.VendingMachine/Company.VendingMachine/VendingMachine.cs
@@ -1,15 +1,95 @@
-﻿namespace Company.VendingMachine
+﻿using System;
+using System.ComponentModel;
+
+namespace Company.VendingMachine
 {
     public class VendingMachine
     {
-        private Coin _penny = new Coin(2,2);
-        private Coin _nickle = new Coin(3, 3);
-        private Coin _dime = new Coin(1, 1);
-        private Coin _quarter = new Coin(4, 4);
+        private readonly Coin _penny = new Coin(2,2);
+        private readonly Coin _nickle = new Coin(3, 3);
+        private readonly Coin _dime = new Coin(1, 1);
+        private readonly Coin _quarter = new Coin(4, 4);
 
-        public bool InsertCoin(Coin coin)
+        private decimal _coinValue;
+        public decimal Amount { get; set; }
+
+
+        public decimal InsertCoin(Coin coin)
         {
-            return coin != _penny;
+            var amount = !ValidCoin(coin) ? 0.00M : CoinValue(coin);
+
+            try
+            {
+                AddAmount(amount);
+                Display(Amount);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error on InsertCoin. Exception:" + e);
+            }
+          
+            return amount;
+        }
+
+        public bool ValidCoin(Coin coin)
+        {
+            if (coin.Weight != _penny.Weight || coin.Size != _penny.Size)
+            {
+                return true;
+            }
+            CoinReturn();
+            return false;
+        }
+
+        public decimal CoinValue(Coin coin)
+        {
+            try
+            {
+                if (coin.Weight != _penny.Weight && coin.Size != _penny.Size)
+                {
+                    if (coin.Weight == _nickle.Weight && coin.Size == _nickle.Size)
+                    {
+                        _coinValue = 0.05M;
+                    }
+                    else if (coin.Weight == _dime.Weight && coin.Size == _dime.Size)
+                    {
+                        _coinValue = 0.10M;
+                    }
+                    else if (coin.Weight == _quarter.Weight && coin.Size == _quarter.Size)
+                    {
+                        _coinValue = 0.25M;
+                    }
+                    else
+                    {
+                        _coinValue = 0.00M;
+                    }
+                }
+                else
+                {
+                    _coinValue = 0.01M;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error on CoinValue. Exception:" + e);
+            }
+
+            return _coinValue;
+        }
+
+        public bool CoinReturn()
+        {
+            return true;
+        }
+
+        public void AddAmount(decimal amount)
+        {
+            Amount += amount;
+        }
+
+        public string Display(decimal amount)
+        {
+            return amount != 0.00M ? Amount.ToString() : "INSERT COIN";
         }
     }
 }
